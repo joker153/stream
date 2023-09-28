b2542e06de9b3e9c996750dbdd7322334fe2e974
joker153 <77005994+joker153@users.noreply.github.com>
2023-09-28 19:44:36 +0530

    Update approve.py

--- plugins/helper/approve.py
+++ plugins/helper/approve.py
@@ -2,14 +2,23 @@ import os
 import asyncio
 from pyrogram import Client, filters
 from pyrogram.types import Message, User, ChatJoinRequest
-from info import CHAT_ID, TEXT, APPROVED 
+from info import CHAT_ID, TEXT, APPROVED, WELCOME_TEXT, JOIN_CHANNEL_TEXT, JOIN_CHANNEL_LINK, AUTH_CHANNEL,
 
 
-@Client.on_chat_join_request((filters.group | filters.channel) & filters.chat(CHAT_ID) if CHAT_ID else (filters.group | filters.channel))
-async def autoapprove(client, message: ChatJoinRequest):
-    chat=message.chat 
-    user=message.from_user 
-    print(f"{user.first_name} Joined (Approved)") 
-    await client.approve_chat_join_request(chat_id=chat.id, user_id=user.id)
-    if APPROVED == "on":
-        await client.send_message(chat_id=chat.id, text=TEXT.format(mention=user.mention, title=chat.title))
+@Client.on_chat_join_request(filters.group | filters.channel)
+async def custom_autoapprove(client: Client, message: ChatJoinRequest):
+    chat = message.chat
+    user = message.from_user
+    print(f"{user.first_name} Joined 🤝")  # Logs
+
+    # Define the specific channel's chat_id to exclude
+    excluded_channel_chat_id = AUTH_CHANNEL
+
+    if chat.id != excluded_channel_chat_id:
+        await client.approve_chat_join_request(chat_id=chat.id, user_id=user.id)
+        if APPROVED == "on":
+            welcome_text = WELCOME_TEXT.format(mention=user.mention, title=chat.title)
+            button = None
+            if JOIN_CHANNEL_LINK:
+                button = InlineKeyboardMarkup([[InlineKeyboardButton(JOIN_CHANNEL_TEXT, url=JOIN_CHANNEL_LINK)]])
+            await client.send_message(chat_id=user.id, text=welcome_text, reply_markup=button)
