4811f711d40cde86bb5cd9662ec6be38a5d1e5f6
joker153 <77005994+joker153@users.noreply.github.com>
2023-09-29 02:34:37 +0530

    Add files via upload

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -290,8 +290,8 @@ async def next_page(bot, query):
 # Language Code Temp
 @Client.on_callback_query(filters.regex(r"^qualities#"))
 async def qualities_cb_handler(client: Client, query: CallbackQuery):
-if query.message.chat.type not in ['group', 'supergroup']:
-    if int(query.from_user.id) != query.message.reply_to_message.from_user.id:
+    if query.message.chat.type not in ['group', 'supergroup']:
+       if int(query.from_user.id) != query.message.reply_to_message.from_user.id:
         return await query.answer(
             f"⚠️ ʜᴇʟʟᴏ {query.from_user.first_name},\nᴛʜɪꜱ ɪꜱ ɴᴏᴛ ʏᴏᴜʀ ᴍᴏᴠɪᴇ ʀᴇQᴜᴇꜱᴛ,\nʀᴇQᴜᴇꜱᴛ ʏᴏᴜʀ'ꜱ...",
             show_alert=True,
@@ -477,9 +477,9 @@ if query.message.chat.type in ['group', 'supergroup']:
 
 @Client.on_callback_query(filters.regex(r"^languages#"))
 async def languages_cb_handler(client: Client, query: CallbackQuery):
-if query.message.chat.type not in ['group', 'supergroup']:
-    if int(query.from_user.id) != query.message.reply_to_message.from_user.id:
-        return await query.answer(
+    if query.message.chat.type not in ['group', 'supergroup']:
+       if int(query.from_user.id) != query.message.reply_to_message.from_user.id:
+           lreturn await query.answer(
             f"⚠️ ʜᴇʟʟᴏ {query.from_user.first_name},\nᴛʜɪꜱ ɪꜱ ɴᴏᴛ ʏᴏᴜʀ ᴍᴏᴠɪᴇ ʀᴇQᴜᴇꜱᴛ,\nʀᴇQᴜᴇꜱᴛ ʏᴏᴜʀ'ꜱ...",
             show_alert=True,
         )
@@ -662,8 +662,8 @@ if query.message.chat.type in ['group', 'supergroup']:
 
 @Client.on_callback_query(filters.regex(r"^seasons#"))
 async def seasons_cb_handler(client: Client, query: CallbackQuery):
-if query.message.chat.type not in ['group', 'supergroup']:
-    if int(query.from_user.id) != query.message.reply_to_message.from_user.id:
+    if query.message.chat.type not in ['group', 'supergroup']:
+       if int(query.from_user.id) != query.message.reply_to_message.from_user.id:
         return await query.answer(
             f"⚠️ ʜᴇʟʟᴏ {query.from_user.first_name},\nᴛʜɪꜱ ɪꜱ ɴᴏᴛ ʏᴏᴜʀ ᴍᴏᴠɪᴇ ʀᴇQᴜᴇꜱᴛ,\nʀᴇQᴜᴇꜱᴛ ʏᴏᴜʀ'ꜱ...",
             show_alert=True,
