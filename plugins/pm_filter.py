13ccd67f0a076c913a521108e13a30678e525246
joker153 <77005994+joker153@users.noreply.github.com>
2023-09-30 15:18:00 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -155,10 +155,15 @@ async def pm_text(bot, message):
 
 
 @Client.on_callback_query(filters.regex(r"^next"))
+@Client.on_callback_query(filters.regex(r"^next"))
 async def next_page(bot, query):
-    ident, req, key, offset = query.data.split("_")
-    if int(req) not in [query.from_user.id, 0]:
+    data_parts = query.data.split("_")
+
+    # Check if there are enough parts to unpack
+    if len(data_parts) < 4:
         return await query.answer(script.ALRT_TXT.format(query.from_user.first_name), show_alert=True)
+
+    ident, req, key, offset = data_parts
     try:
         offset = int(offset)
     except:
