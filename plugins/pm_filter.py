ce1faaba6c4a7bf8764f68e4e3916e4cdbb33c6b
joker153 <77005994+joker153@users.noreply.github.com>
2023-10-08 16:48:28 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -894,14 +894,12 @@ async def filter_episodes_cb_handler(client: Client, query: CallbackQuery):
         ),
     ])
 
-
-
     # Add an option to go back to the seasons
     episode_buttons.append([InlineKeyboardButton(text="⬅ Back to Seasons", callback_data=f"seasons#{search}#{key}")])
 
-    
     # Edit the message to show episode buttons
     await query.edit_message_reply_markup(reply_markup=InlineKeyboardMarkup(btn))
+
 # spellcheck error fixing
 @Client.on_callback_query(filters.regex(r"^spol"))
 async def advantage_spoll_choker(bot, query):
