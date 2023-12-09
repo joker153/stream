01adb764626ac1519b394775b4f6ea56d5d32cb7
joker153 <77005994+joker153@users.noreply.github.com>
2023-12-09 19:44:28 +0530

    Update join_reqs.py

--- database/join_reqs.py
+++ database/join_reqs.py
@@ -3,7 +3,7 @@
 # (c) @AlbertEinsteinTG
 
 import motor.motor_asyncio
-from info import REQ_CHANNEL
+from info import AUTH_CHANNEL
 
 class JoinReqs:
 
@@ -12,7 +12,7 @@ class JoinReqs:
         if JOIN_REQS_DB:
             self.client = motor.motor_asyncio.AsyncIOMotorClient(JOIN_REQS_DB)
             self.db = self.client["JoinReqs"]
-            self.col = self.db[str(REQ_CHANNEL)]
+            self.col = self.db[str(AUTH_CHANNEL)]
         else:
             self.client = None
             self.db = None
