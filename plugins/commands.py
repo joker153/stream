263f0b5de99b50574b339ce3712a5b12e67cb3eb
joker153 <77005994+joker153@users.noreply.github.com>
2024-03-17 23:20:52 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -55,34 +55,34 @@ async def start(client, message):
             parse_mode=enums.ParseMode.HTML
         )
         return
-if AUTH_CHANNEL and not await is_subscribed(client, message):
-  try:
-    invite_link = await client.create_chat_invite_link(int(AUTH_CHANNEL))
-  except ChatAdminRequired:
-    logger.error("Make sure Bot is admin in Forcesub channel")
-    return
-  btn = [
-    [
-      InlineKeyboardButton(
-          " Join Updates Channel", url=invite_link.invite_link
-      )
-    ]
-  ]
+    if AUTH_CHANNEL and not await is_subscribed(client, message):
+        try:
+            invite_link = await client.create_chat_invite_link(int(AUTH_CHANNEL))
+        except ChatAdminRequired:
+            logger.error("Make sure Bot is admin in Forcesub channel")
+            return
+        btn = [
+            [
+                InlineKeyboardButton(
+                    "🤖 Join Updates Channel", url=invite_link.invite_link
+                )
+            ]
+        ]
 
-  if message.command[1] != "subscribe":
-    try:
-      kk, file_id = message.command[1].split("_", 1)
-      pre = 'checksubp' if kk == 'filep' else 'checksub'
-      btn.append([InlineKeyboardButton("  Try Again", callback_data=f"{pre}#{file_id}")])
-    except (IndexError, ValueError):
-      btn.append([InlineKeyboardButton("  Try Again", url=f"https://t.me/{temp.U_NAME}?start={message.command[1]}")])
-  await client.send_message(
-      chat_id=message.from_user.id,
-      text="**Please Join My Updates Channel to use this Bot!**",
-      reply_markup=InlineKeyboardMarkup(btn),
-      parse_mode=enums.ParseMode.MARKDOWN
-  )
-  return
+        if message.command[1] != "subscribe":
+            try:
+                kk, file_id = message.command[1].split("_", 1)
+                pre = 'checksubp' if kk == 'filep' else 'checksub' 
+                btn.append([InlineKeyboardButton(" 🔄 Try Again", callback_data=f"{pre}#{file_id}")])
+            except (IndexError, ValueError):
+                btn.append([InlineKeyboardButton(" 🔄 Try Again", url=f"https://t.me/{temp.U_NAME}?start={message.command[1]}")])
+        await client.send_message(
+            chat_id=message.from_user.id,
+            text="**Please Join My Updates Channel to use this Bot!**",
+            reply_markup=InlineKeyboardMarkup(btn),
+            parse_mode=enums.ParseMode.MARKDOWN
+            )
+        return
     if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[
             InlineKeyboardButton('sᴜʀᴘʀɪsᴇ', callback_data='start')
