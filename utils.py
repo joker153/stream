224a1d0ac0e32015c51c036c6194d546e8261822
joker153 <77005994+joker153@users.noreply.github.com>
2024-03-17 18:39:59 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -44,18 +44,19 @@ class temp(object):
     SETTINGS = {}
 
 
-async def is_subscribed(bot, query):
-    try:
-        user = await bot.get_chat_member(AUTH_CHANNEL, query.from_user.id)
-    except UserNotParticipant:
-        pass
-    except Exception as e:
-        logger.exception(e)
-    else:
-        if user.status != 'kicked':
-            return True
-
-    return False
+async def is_subscribed(bot, query, channel):
+    btn = []
+    for id in channel:
+        chat = await bot.get_chat(int(id))
+        try:
+            await bot.get_chat_member(id, query.from_user.id)
+        except UserNotParticipant:
+            btn.append(
+                [InlineKeyboardButton(f'Join {chat.title}', url=chat.invite_link)]
+            )
+        except Exception as e:
+            pass
+    return btn
 
 async def is_subscribed(bot, query):
     try:
