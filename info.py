99b7303091807f52181bce6a23754c18d52cdea0
joker153 <77005994+joker153@users.noreply.github.com>
2024-03-17 19:57:20 +0530

    Update info.py

--- info.py
+++ info.py
@@ -85,6 +85,7 @@ DOWNLOAD_LOCATION = environ.get("DOWNLOAD_LOCATION", "./DOWNLOADS/AudioBoT/")
 # FSUB
 
 auth_channel = environ.get('AUTH_CHANNEL')
+AUTH_CHANNEL2 = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else False
 AUTH_CHANNEL = [int(auth_channels) for auth_channels in environ.get('AUTH_CHANNEL', '').split()]
 if len(AUTH_CHANNEL) == 0:
     print('Info - AUTH_CHANNEL is empty')
