c72d5457d05ef7c55ccdbba071575d4ce07a1286
joker153 <77005994+joker153@users.noreply.github.com>
2024-03-17 22:03:46 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -49,7 +49,7 @@ async def is_subscribed(bot, query, channel):
     for id in channel:
         chat = await bot.get_chat(int(id))
         try:
-            await bot.get_chat_member(id, query.from_user.id)
+            await bot.get_chat_member(AUTH_CHANNEL, query.from_user.id)
         except UserNotParticipant:
             btn.append(
                 [InlineKeyboardButton(f'Join {chat.title}', url=chat.invite_link)]
