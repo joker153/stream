acceeb3f3cffa0f572405f82d28792cdeadfbc08
joker153 <77005994+joker153@users.noreply.github.com>
2024-06-14 19:51:26 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/joker153/zoro /zoro
+  git clone https://github.com/joker153/zoroflm.git /zoroflm
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /zoro
+  git clone $UPSTREAM_REPO /zoroflm
 fi
 cd /zoro
 pip3 install -U -r requirements.txt
