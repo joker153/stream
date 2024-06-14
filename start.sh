2e18acf00f468e10463c624cd63bb551ba20ec08
joker153 <77005994+joker153@users.noreply.github.com>
2024-06-14 13:21:05 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/joker153/elsam.git /elsam
+  git clone https://github.com/joker153/zoro /zoro
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /elsam
+  git clone $UPSTREAM_REPO /zoro
 fi
-cd /elsam
+cd /zoro
 pip3 install -U -r requirements.txt
-echo "𝙎𝙩𝙖𝙧𝙩𝙞𝙣𝙜 𝙀𝙡𝙨𝙖....🧞‍♂️"
+echo "𝙎𝙩𝙖𝙧𝙩𝙞𝙣𝙜 zoro....🧞‍♂️"
 python3 bot.py
